bb4ab886861d4439d8c62bc74973d04a86003f53
EnusSoul <104706377+ENUSSOUL001@users.noreply.github.com>
2025-08-03 20:46:05 +0800

    Update TextHelper.cs

--- AskAI/TextHelper.cs
+++ AskAI/TextHelper.cs
@@ -11,6 +11,8 @@ namespace AskAI
         public static readonly Color ErrorColor = new Color(255, 100, 100);
         public static readonly Color UsageColor = new Color(230, 230, 230);
         public static readonly Color UsageParamColor = new Color(180, 180, 180);
+        public static readonly Color GreetColor = new Color(0, 255, 150);
+        public static readonly Color GreetHeaderColor = new Color(150, 255, 200);
 
         private static readonly Regex ColorTagRegex = new Regex(@"\[c\/(?<color>[0-9A-Fa-f]{6}):(?<text>.*?)\]", RegexOptions.Singleline);
 
@@ -32,7 +34,7 @@ namespace AskAI
                 }
 
                 var parts = content.Split('\n');
-                return string.Join("\n", parts.Select(p => $"[c/{color}:{p}]"));
+                return string.Join("\n", parts.Select(p => string.IsNullOrWhiteSpace(p) ? p : $"[c/{color}:{p}]"));
             });
         }
     }
