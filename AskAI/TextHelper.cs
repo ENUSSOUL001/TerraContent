08dcafbe016e8dac03ac5ba3f5f1cb4581aa35e2
EnusSoul <104706377+ENUSSOUL001@users.noreply.github.com>
2025-08-03 18:21:44 +0800

    Update TextHelper.cs

--- AskAI/TextHelper.cs
+++ AskAI/TextHelper.cs
@@ -6,13 +6,9 @@ namespace AskAI
     {
         public static readonly Color AINameColor = new Color(173, 216, 230);
         public static readonly Color InfoColor = Color.Yellow;
+        public static readonly Color SuccessColor = new Color(144, 238, 144);
         public static readonly Color ErrorColor = new Color(255, 100, 100);
         public static readonly Color UsageColor = new Color(230, 230, 230);
         public static readonly Color UsageParamColor = new Color(180, 180, 180);
-
-        public static string Colorize(string text, Color color)
-        {
-            return $"[c/{color.R:X2}{color.G:X2}{color.B:X2}:{text}]";
-        }
     }
 }
