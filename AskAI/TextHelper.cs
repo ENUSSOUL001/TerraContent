918c00421b7363f6c291493e72d16a402387096a
EnusSoul <104706377+ENUSSOUL001@users.noreply.github.com>
2025-08-04 21:51:08 +0800

    Update TextHelper.cs

--- AskAI/TextHelper.cs
+++ AskAI/TextHelper.cs
@@ -1,5 +1,4 @@
 using Microsoft.Xna.Framework;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace AskAI
@@ -9,32 +8,25 @@ namespace AskAI
         public static readonly Color AINameColor = new Color(173, 216, 230);
         public static readonly Color InfoColor = Color.Yellow;
         public static readonly Color ErrorColor = new Color(255, 100, 100);
-        public static readonly Color UsageColor = new Color(230, 230, 230);
-        public static readonly Color UsageParamColor = new Color(180, 180, 180);
+        public static readonly Color UsageColor = new Color(0, 255, 150);
+        public static readonly Color UsageParamColor = new Color(150, 255, 200);
         public static readonly Color GreetColor = new Color(0, 255, 150);
         public static readonly Color GreetHeaderColor = new Color(150, 255, 200);
-
-        private static readonly Regex ColorTagRegex = new Regex(@"\[c\/(?<color>[0-9A-Fa-f]{6}):(?<text>.*?)\]", RegexOptions.Singleline);
 
         public static string Colorize(string text, Color color)
         {
             return $"[c/{color.R:X2}{color.G:X2}{color.B:X2}:{text}]";
         }
 
-        public static string FixMalformedColorTags(string text)
+        private static readonly Regex AICodeRegex = new Regex(@"<(?<color>[0-9A-Fa-f]{6}):(?<text>.*?)>", RegexOptions.Singleline);
+        public static string ConvertAiColorTags(string text)
         {
-            return ColorTagRegex.Replace(text, match =>
+            return AICodeRegex.Replace(text, match =>
             {
                 var color = match.Groups["color"].Value;
                 var content = match.Groups["text"].Value;
-
-                if (!content.Contains('\n'))
-                {
-                    return match.Value;
-                }
-
-                var parts = content.Split('\n');
-                return string.Join("\n", parts.Select(p => string.IsNullOrWhiteSpace(p) ? p : $"[c/{color}:{p}]"));
+                var nestedContent = ConvertAiColorTags(content);
+                return $"[c/{color}:{nestedContent}]";
             });
         }
     }
