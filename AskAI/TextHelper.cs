db1929b7845b88bbca4823bc43b58fa089be0cbe
EnusSoul <104706377+ENUSSOUL001@users.noreply.github.com>
2025-08-06 20:12:47 +0800

    Update TextHelper.cs

--- AskAI/TextHelper.cs
+++ AskAI/TextHelper.cs
@@ -17,20 +17,108 @@ namespace AskAI
         public static readonly Color GreetHeaderColor = new Color(150, 255, 200);
 
         private enum TagType { Colon, Hash }
-        private struct TagInfo
+        private enum TokenType { PlainText, OpenTag, CloseTag, NewLine }
+
+        private struct Token
         {
-            public readonly string Color;
-            public readonly TagType Type;
-            public TagInfo(string color, TagType type)
+            public readonly TokenType Type;
+            public readonly string Content;
+            public readonly TagType TagStyle;
+
+            public Token(TokenType type, string content = "", TagType tagStyle = TagType.Colon)
             {
-                Color = color;
                 Type = type;
+                Content = content;
+                TagStyle = tagStyle;
             }
         }
 
         public static string Colorize(string text, Color color)
         {
             return $"[c/{color.R:X2}{color.G:X2}{color.B:X2}:{text}]";
+        }
+
+        private static List<Token> Tokenize(ReadOnlySpan<char> input)
+        {
+            var tokens = new List<Token>();
+            var textBuffer = new StringBuilder();
+            int i = 0;
+
+            while (i < input.Length)
+            {
+                if (input[i] == '\n')
+                {
+                    if (textBuffer.Length > 0)
+                    {
+                        tokens.Add(new Token(TokenType.PlainText, textBuffer.ToString()));
+                        textBuffer.Clear();
+                    }
+                    tokens.Add(new Token(TokenType.NewLine));
+                    i++;
+                    continue;
+                }
+
+                if (input[i] == '<')
+                {
+                    if (i + 8 < input.Length && input[i + 7] == ':' && IsHexString(input.Slice(i + 1, 6)))
+                    {
+                        if (textBuffer.Length > 0)
+                        {
+                            tokens.Add(new Token(TokenType.PlainText, textBuffer.ToString()));
+                            textBuffer.Clear();
+                        }
+                        tokens.Add(new Token(TokenType.OpenTag, input.Slice(i + 1, 6).ToString(), TagType.Colon));
+                        i += 8;
+                        continue;
+                    }
+                    
+                    if (i + 8 < input.Length && input[i + 1] == '#' && input[i + 8] == '>' && IsHexString(input.Slice(i + 2, 6)))
+                    {
+                        if (textBuffer.Length > 0)
+                        {
+                            tokens.Add(new Token(TokenType.PlainText, textBuffer.ToString()));
+                            textBuffer.Clear();
+                        }
+                        tokens.Add(new Token(TokenType.OpenTag, input.Slice(i + 2, 6).ToString(), TagType.Hash));
+                        i += 9;
+                        continue;
+                    }
+
+                    if (i + 3 < input.Length && input.Slice(i, 4).SequenceEqual("</#>".AsSpan()))
+                    {
+                        if (textBuffer.Length > 0)
+                        {
+                            tokens.Add(new Token(TokenType.PlainText, textBuffer.ToString()));
+                            textBuffer.Clear();
+                        }
+                        tokens.Add(new Token(TokenType.CloseTag, tagStyle: TagType.Hash));
+                        i += 4;
+                        continue;
+                    }
+                }
+
+                if (input[i] == '>')
+                {
+                    if (textBuffer.Length > 0)
+                    {
+                        tokens.Add(new Token(TokenType.PlainText, textBuffer.ToString()));
+                        textBuffer.Clear();
+                    }
+                    tokens.Add(new Token(TokenType.CloseTag, tagStyle: TagType.Colon));
+                    i++;
+                    continue;
+                }
+
+                textBuffer.Append(input[i]);
+                i++;
+            }
+
+            if (textBuffer.Length > 0)
+            {
+                tokens.Add(new Token(TokenType.PlainText, textBuffer.ToString()));
+            }
+
+            return tokens;
         }
 
         public static string ConvertAiColorTags(string inputText)
@@ -40,90 +128,50 @@ namespace AskAI
                 return string.Empty;
             }
 
+            var tokens = Tokenize(inputText.AsSpan());
             var result = new StringBuilder(inputText.Length + 100);
-            var tagStack = new Stack<TagInfo>();
-            var span = inputText.AsSpan();
+            var tagStack = new Stack<TagType>();
 
-            for (int i = 0; i < span.Length; i++)
+            foreach (var token in tokens)
             {
-                char currentChar = span[i];
-
-                if (currentChar == '<')
+                switch (token.Type)
                 {
-                    if (i + 1 < span.Length && span[i + 1] == '#')
-                    {
-                        if (i + 3 < span.Length && span[i + 2] == '/' && span[i + 3] == '>')
+                    case TokenType.PlainText:
+                        result.Append(token.Content);
+                        break;
+
+                    case TokenType.OpenTag:
+                        result.Append($"[c/{token.Content}:");
+                        tagStack.Push(token.TagStyle);
+                        break;
+
+                    case TokenType.CloseTag:
+                        if (tagStack.Count > 0 && tagStack.Peek() == token.TagStyle)
                         {
-                            if (tagStack.Count > 0 && tagStack.Peek().Type == TagType.Hash)
-                            {
-                                tagStack.Pop();
-                                result.Append(']');
-                            }
-                            i += 3;
-                            continue;
+                            tagStack.Pop();
+                            result.Append(']');
                         }
-                        
-                        if (i + 8 < span.Length && span[i + 8] == '>')
+                        break;
+
+                    case TokenType.NewLine:
+                        var reversedStack = new Stack<TagType>();
+                        while (tagStack.Count > 0)
                         {
-                            var colorSlice = span.Slice(i + 2, 6);
-                            if (IsHexString(colorSlice))
-                            {
-                                string color = colorSlice.ToString();
-                                tagStack.Push(new TagInfo(color, TagType.Hash));
-                                result.Append($"[c/{color}:");
-                                i += 8;
-                                continue;
-                            }
+                            result.Append(']');
+                            reversedStack.Push(tagStack.Pop());
                         }
-                    }
-                    
-                    if (i + 8 < span.Length && span[i + 7] == ':')
-                    {
-                        var colorSlice = span.Slice(i + 1, 6);
-                        if (IsHexString(colorSlice))
+                        result.Append('\n');
+                        while (reversedStack.Count > 0)
                         {
-                            string color = colorSlice.ToString();
-                            tagStack.Push(new TagInfo(color, TagType.Colon));
+                            var tagType = reversedStack.Pop();
+                            var color = result.ToString().Substring(result.ToString().LastIndexOf($"[c/") + 3, 6);
                             result.Append($"[c/{color}:");
-                            i += 7;
-                            continue;
+                            tagStack.Push(tagType);
                         }
-                    }
+                        break;
                 }
-
-                if (currentChar == '>')
-                {
-                    if (tagStack.Count > 0 && tagStack.Peek().Type == TagType.Colon)
-                    {
-                        tagStack.Pop();
-                        result.Append(']');
-                        continue;
-                    }
-                }
-                
-                if (currentChar == '\n')
-                {
-                    var reversedStack = new Stack<TagInfo>(tagStack.Count);
-                    while (tagStack.Count > 0)
-                    {
-                        result.Append(']');
-                        reversedStack.Push(tagStack.Pop());
-                    }
-
-                    result.Append('\n');
-                    
-                    while (reversedStack.Count > 0)
-                    {
-                        var tagInfo = reversedStack.Pop();
-                        result.Append($"[c/{tagInfo.Color}:");
-                        tagStack.Push(tagInfo);
-                    }
-                    continue;
-                }
-                
-                result.Append(currentChar);
             }
-            
+
             while (tagStack.Count > 0)
             {
                 result.Append(']');
