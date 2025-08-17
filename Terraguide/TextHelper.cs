0593b1ffaa3301e8f3b6938841ceb4549256822c
EnusSoul <104706377+ENUSSOUL001@users.noreply.github.com>
2025-08-17 16:58:39 +0800

    Update TextHelper.cs

--- Terraguide/TextHelper.cs
+++ Terraguide/TextHelper.cs
@@ -5,13 +5,9 @@ namespace TerraGuide
     public static class TextHelper
     {
         public static string ColorHeader(string text) => $"[c/FFD700:{text}]";
-
         public static string ColorRecipeName(string text) => $"[c/87CEEB:{text}]";
-
         public static string ColorStation(string text) => $"[c/98FB98:{text}]";
-
         public static string ColorItem(string text) => $"[c/DDA0DD:{text}]";
-
         public static string ColorRequirement(string text) => $"[c/FF6B6B:{text}]";
     }
 }
